66b1b4b982a40ce68fb18a8c3d3ed02af5bd7902
cent kanayo <centkanayo@gmail.com>
2022-11-27 08:40:30 +0000

    new commit

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const cors = require('cors');
 const server = express();
 const PORT = 3500;
 
-server.use(cors());
+server.all('/*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+
+  next();
+});
+
 server.get('/home', (req, res) => {
   res.status(200).json({ msg: 'TALENT PLUS' });
 });
