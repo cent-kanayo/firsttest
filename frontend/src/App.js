4e90ece916c952445050c73e859fd341c3d0a1d4
cent kanayo <centkanayo@gmail.com>
2022-11-28 11:42:27 +0000

    new commit

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchMessage = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get('http://backend-svc/home');
+        setMessage(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+        console.log(error);
+      }
+    };
+    fetchMessage();
+  }, []);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>Oops! Something went wrong, please try again</h1>;
+  return (
+    <div>
+      <header className="padding">
+        <h1>DevOps</h1>
+      </header>
+      <div className="grid">
+        <h3>The text below is coming from the backend</h3>
+        <h1>{message?.msg}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
